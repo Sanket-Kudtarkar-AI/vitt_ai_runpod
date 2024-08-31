06fd4e2e9a542973df4838c1730f072244021dbf
Sanket <skudtarkar16@gmail.com>
2024-08-31 21:06:23 +0530

    Optimized handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,21 +1,51 @@
 """ Example handler file. """
 import runpod
-from llm_token_streaming import function_stream
-
-from flask import jsonify
+from openai import OpenAI
 
 print(50 * "*")
-print("starting handler.py")
+print("starting handler.py v2")
 print(50 * "*")
+openai_api_key = "EMPTY"
+vllm_server = "http://0.0.0.0:5001/v1/"
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=vllm_server,
+)
 
 
 async def process_request(job):
     try:
         query = job['input']['query']
+
         if not query:
             return {"status": "error", "message": "Query parameter is missing"}
 
-        llm_response = await function_stream(prompt=query)
+        # models = client.models.list()
+        # model = models.data[0].id
+
+        prompt_template = (
+            "<|system|>\n"
+            "You are a helpful assistant.<|end|>\n"
+            "<|user|>\n"
+            "{query}<|end|>\n"
+            "<|assistant|>"
+        )
+        formatted_prompt = prompt_template.format(query=query)
+
+        completion_params = {
+            "model": "/models/microsoft/Phi-3.5-mini-instruct",
+            "prompt": formatted_prompt,
+            "echo": False,
+            "stream": False,
+            "temperature": 0.0,
+            "max_tokens": 500,
+            "stop": ["<|end|>"]
+        }
+
+        completion = client.completions.create(**completion_params)
+        llm_response = completion.choices[0].text
+        # llm_response = await function_stream(prompt=query)
         return {"status": "success", "llm_response": llm_response}
 
     except Exception as e:
@@ -25,7 +55,7 @@ async def process_request(job):
 
 # Start the serverless function with the handler and concurrency modifier
 runpod.serverless.start(
-    {"handler": process_request, "concurrency_modifier": lambda x: 100}
+    {"handler": process_request, "concurrency_modifier": lambda x: 10}
 )
 
 # python handler.py --test_input '{"input":{"query": "The quick brown fox jumps"}}'
